dc53056a5e25bbd9718e1f5f19293a2f3b8ebd8c
Sherzod Uralov <sherzoduralov22@gmail.com>
2025-12-27 14:44:24 +0500

    Update _config.py

--- _config.py
+++ _config.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-import datetime
-import plotly.express as px
 
 # 1. SAHIFA SOZLAMALARI
 st.set_page_config(page_title="Call Center Analysis", layout="wide")
